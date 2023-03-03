27db4d5e36f7bb90473987f2847565641f558e53
puregyu <puregyu@naver.com>
2023-03-03 22:00:00 +0900

    Feat : "index 2"

--- effective-java/README.md
+++ effective-java/README.md
@@ -1,3 +1,4 @@
 # effective java 3 Edition
 
 #### 1. [생성자 대신 정적 팩터리 메서드를 고려하라](src/main/java/com/devyu/effectivejava/no1/Main.java)
+#### 1. [생성자에 매개변수가 많다면 빌더를 고려하라](src/main/java/com/devyu/effectivejava/no2/Main.java)
---
+++ effective-java/src/main/java/com/devyu/effectivejava/no2/Store.java
@@ -0,0 +1,100 @@
+package com.devyu.effectivejava.no2;
+
+public class Store {
+
+    private String enterpriseCode;
+    private String corporationCode;
+    private String storeCode;
+
+    // ===== ===== ===== ===== =====
+    // 생성자 패턴
+    // ===== ===== ===== ===== =====
+
+    // 매개변수가 없는 construct
+    public Store() {
+    }
+
+    // 하나의 매개변수만 가지고 있는 constructor
+    public Store(String enterpriseCode) {
+        this.enterpriseCode = enterpriseCode;
+    }
+
+    // 두개의 매개변수만 가지고 있는 constructor
+    public Store(String enterpriseCode, String corporationCode) {
+        this.enterpriseCode = enterpriseCode;
+        this.corporationCode = corporationCode;
+    }
+
+    // 모든 매개변수를 가지고 있는 constructor
+    public Store(String enterpriseCode, String corporationCode, String storeCode) {
+        this.enterpriseCode = enterpriseCode;
+        this.corporationCode = corporationCode;
+        this.storeCode = storeCode;
+    }
+
+    // ===== ===== ===== ===== =====
+    // 자바빈즈 패턴
+    // ===== ===== ===== ===== =====
+
+    public String getEnterpriseCode() {
+        return enterpriseCode;
+    }
+
+    public void setEnterpriseCode(String enterpriseCode) {
+        this.enterpriseCode = enterpriseCode;
+    }
+
+    public String getCorporationCode() {
+        return corporationCode;
+    }
+
+    public void setCorporationCode(String corporationCode) {
+        this.corporationCode = corporationCode;
+    }
+
+    public String getStoreCode() {
+        return storeCode;
+    }
+
+    public void setStoreCode(String storeCode) {
+        this.storeCode = storeCode;
+    }
+
+    // ===== ===== ===== ===== =====
+    // 빌더 패턴
+    // ===== ===== ===== ===== =====
+    public static class Builder {
+
+        private String enterpriseCode;
+        private String corporationCode;
+        private String storeCode;
+
+        public Builder() {
+        }
+
+        public Builder enterpriseCode(String enterpriseCode) {
+            return this;
+        }
+
+        public Builder corporationCode(String corporationCode) {
+            return this;
+        }
+
+        public Builder storeCode(String storeCode) {
+            return this;
+        }
+
+        public Store build() {
+            return new Store(this);
+        }
+
+    }
+
+    // 생성자를 private으로 선언
+    private Store(Builder builder) {
+        enterpriseCode = builder.enterpriseCode;
+        corporationCode = builder.corporationCode;
+        storeCode = builder.storeCode;
+    }
+
+}
